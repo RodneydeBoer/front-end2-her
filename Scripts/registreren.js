07ff48519582ef9cbd38366da8250a0c78ffeaaa
RodneydeBoer <rodneydeboer1@hotmail.com>
2021-06-04 10:42:25 +0200

    Small changes

--- Scripts/registreren.js
+++ Scripts/registreren.js
@@ -44,3 +44,32 @@ form.addEventListener('keydown', function() {
         submit.style.display = "block";
     }
 });
+
+// Storing the login-data to local storage to check at login
+function store() {
+
+    if (name.value.length == 0) {
+        alert('Please fill in email');
+
+    } else {
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('pw', pw.value);
+        alert('Your account has been created');
+    }
+}
+
+//checking
+function check() {
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+    var userRemember = document.getElementById("rememberMe");
+
+    if (userName.value == storedName && userPw.value == storedPw) {
+        alert('You are logged in.');
+    } else {
+        alert('Error on login');
+    }
+}
